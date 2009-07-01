1ae7a56d9031855eabcf15eb78c9cca0a61bd202
pawel <pawel@charon.(none)>
2009-07-01 16:45:50 +0200

    Some work on saving twills in the database.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,14 +2,68 @@
 
 from __future__ import absolute_import
 
+import sqlite3
 from nose.tools import *
 
-from twillmanager.watcher import Watcher
+from twillmanager.watcher import Watch
 
 
-class Test_Watcher(object):
-    def test_watcher_can_be_created(self):
-        w = Watcher("Code Sprinters", "go http://www.codesprinters.com/", 120)
-        assert_equal(120, w.interval)
-        assert_equal("go http://www.codesprinters.com/", w.script)
-        assert_equal("Code Sprinters", w.name)
+class Test_Watch(object):
+    def setUp(self):
+        self.connection = sqlite3.connect(':memory:')
+        c = self.connection.cursor()
+        c.execute("""CREATE TABLE twills(
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(255) UNIQUE NOT NULL,
+            interval INTEGER NOT NULL,
+            script TEXT,
+            status VARCHAR(100) NOT NULL,
+            time INTEGER)""")
+        c.close()
+
+    def test_saving(self):
+        w1 = Watch('codesprinters', 10, "go codesprinters")
+        w1.save(self.connection)
+        w2 = Watch('google', 10, "go google")
+        w2.save(self.connection)
+
+        w1loaded = Watch.load('codesprinters', self.connection)
+        w2loaded = Watch.load('google', self.connection)
+
+        assert_equal(w1.name, w1loaded.name)
+        assert_equal(w1.script, w1loaded.script)
+        assert_equal(w1.interval, w1loaded.interval)
+
+        assert_equal(w2.name, w2loaded.name)
+        assert_equal(w2.script, w2loaded.script)
+        assert_equal(w2.interval, w2loaded.interval)
+
+    def test_updating(self):
+        w1 = Watch('codesprinters', 10, "go codesprinters")
+        w1.save(self.connection)
+        w2 = Watch('google', 10, "go google")
+        w2.save(self.connection)
+
+        w1loaded = Watch.load('codesprinters', self.connection)
+        w2loaded = Watch.load('google', self.connection)
+
+        assert_equal(w1.name, w1loaded.name)
+        assert_equal(w1.script, w1loaded.script)
+        assert_equal(w1.interval, w1loaded.interval)
+
+        assert_equal(w2.name, w2loaded.name)
+        assert_equal(w2.script, w2loaded.script)
+        assert_equal(w2.interval, w2loaded.interval)
+
+        w1.script = "go http://codesprinters.com"
+        w1.update(self.connection)
+        w1loaded = Watch.load('codesprinters', self.connection)
+        w2loaded = Watch.load('google', self.connection)
+
+        assert_equal(w1.name, w1loaded.name)
+        assert_equal(w1.script, w1loaded.script)
+        assert_equal(w1.interval, w1loaded.interval)
+
+        assert_equal(w2.name, w2loaded.name)
+        assert_equal(w2.script, w2loaded.script)
+        assert_equal(w2.interval, w2loaded.interval)
--- twillmanager/watcher.py
+++ twillmanager/watcher.py
@@ -1,10 +1,13 @@
 # encoding: utf-8
 
 from __future__ import absolute_import
+from __future__ import with_statement
 
-import time
 from Queue import Empty
-from multiprocessing import Process, Queue, current_process
+from multiprocessing import Process, Queue
+from threading import Lock
+
+from sqlite3 import connect
 
 # Consts for statuses
 STATUS_OK = 'OK'
@@ -13,34 +16,83 @@ STATUS_UNKNOWN = 'UNKNOWN'
 
 class Watch(object):
     """ A simple data transfer object for describing watches """
-    def __init__(self, name, interval, script):
+    def __init__(self, name, interval, script, status=STATUS_UNKNOWN, time=None):
         self.name = name
         self.interval = interval
         self.script = script
-        self.status = STATUS_UNKNOWN # status
-        self.time = None# execution timestamp
+        self.status = status
+        self.time = time
+
+    def save(self, connection):
+        c = connection.cursor()
+        c.execute("INSERT INTO twills (name, interval, script, status, time) VALUES (?,?,?,?,?)",
+            (self.name, self.interval, self.script, self.status, self.time))
+        c.close()
+        connection.commit()
+
+    def update(self, connection):
+        c = connection.cursor()
+        c.execute("UPDATE twills SET interval=?, script=?, status=?, time=? WHERE name = ?",
+            (self.interval, self.script, self.status, self.time, self.name))
+        c.close()
+        connection.commit()
+
+    def delete(self, connection):
+        c = connection.cursor()
+        c.execute("DELETE FROM twills WHERE name = ?", (self.name,))
+        c.close()
+        connection.commit()
+
+    @classmethod
+    def load(self, name, connection):
+        watch = None
+        c = connection.cursor()
+        c.execute("SELECT interval, script, status, time FROM twills WHERE name = ?", (name,))
+        for row in c:
+            watch = Watch(name, *row)
+        c.close()
+        return watch
+
+    @classmethod
+    def load_all(self, connection):
+        watches = []
+        c = connection.cursor()
+        c.execute("SELECT name, interval, script, status, time FROM twills")
+        for row in c:
+            watches.append(Watch(*row))
+        c.close()
+        return watches
 
 class AsyncProcess(object):
     """ A simple abstract class for asynchronously responding server processes """
-    def __init__(self):
+    def __init__(self, tick_interval=None):
         self.queue = Queue(0)
-        self.process = current_process()
+        self.process = Process(target=self.main)
+        self.tick_interval = tick_interval
         self._running = False # this variable is not shared between processes
         
     def spawn(self, daemon=False):
         """ Start in another process """
-        self.process = Process(target=self.start)
         self.process.daemon = daemon
         self.process.start()
 
-    def start(self):
+    def is_alive(self):
+        return self.process.is_alive()
+
+    def main(self):
         try:
             self._running = True
             while self._running:
-                item = self.queue.get()
-                self.execute_command(item)
+                try:
+                    self.execute_command(self.queue.get(True, self.tick_interval))
+                except Empty:
+                    self.tick()
         finally:
             self._running = False
+
+    def tick(self):
+        pass
+        
 
     def queue_command(self, name, *arguments):
         """ Queue a command to be executed by the process"""
@@ -52,117 +104,71 @@ class AsyncProcess(object):
         func = getattr(self, '_' + name)
         return func(*arguments)
 
-class WorkerManager(AsyncProcess):
+class WatchWorker(AsyncProcess):
     """ Object managing spawning of other workers."""
-    def __init__(self):
-        AsyncProcess.__init__(self)
-        self.watches = {}
-        self.workers = {}
-
-    def add(self, watch):
-        """ Asynchronously add a watch.
-
-            :param watch: A `Watch` object
-        """
-        self.queue_command('add', watch)
-        
-    def _add(self, watch):
-        name = watch.name
-        if name in self.watches:
-            raise KeyError("Watch `%s` already defined" % name)
-        print "Adding watch %s" % name
-        worker = Worker(watch, self)
-        worker.spawn(True)
-        self.watches[name] = watch
-        self.workers[name] = worker
-
-    def remove(self, watch):
-        """ Asynchronously remove a watch.
-
-            :param watch: A `Watch` object or watch name (string)
-        """
-        self.queue_command('remove', watch)
-
-    def _remove(self, watch):
-        # support both strings and watch objects
-        if hasattr(watch, 'name'):
-            name = watch.name
-        else:
-            name = watch
-            
-        print "Removing worker %s" % name
-        if name in self.workers:
-            worker = self.workers[name]
-            worker.quit()
-        del self.workers[name]
-        del self.watches[name]
-
-    def quit(self):
-        self.queue_command('quit')
-
-    def _quit(self):
-        print "Stopping manager"
-        self._running = False
-        for worker in self.workers.itervalues():
-            worker.quit()
-        self.workers = {}
-        self.watches = {}
-
-    def update_status(self, name, status):
-        self.queue_command('update_status', name, status)
-
-    def _update_status(self, name, status):
-        if name not in self.watches:
-            return
-
-        old_status = self.watches[name].status
-        self.watches[name].status = status
-        self.watches[name].time = time.time()
-        if old_status != STATUS_UNKNOWN and old_status != status:
-            print "STATUS CHANGED: %s -> %s" % (old_status, status)
-
-    def list(self):
-        back_queue = Queue()
-        self.queue_command('list', back_queue)
-
-        try:
-            return back_queue.get(True, 120)
-        except Empty:
-            return None
-    
-    def _list(self, back_queue):
-        back_queue.put(self.watches)
-            
-
-class Worker(AsyncProcess):
-    """ Object managing spawning of other workers."""
-    def __init__(self, watch, manager):
-        AsyncProcess.__init__(self)
+    def __init__(self, watch, manager, connstring):
+        AsyncProcess.__init__(self, watch.interval)
         self.watch = watch
         self.manager = manager
+        self.connection = None
+        self.connstring = connstring
 
-    def start(self):
-        try:
-            self._running = True
-            while self._running:
-                try:
-                    print self.queue
-                    item = self.queue.get(True, self.watch.interval)
-                    self.execute_command(item)
-                except Empty:
-                    self._execute()
-        finally:
-            self._running = False
+    def main(self):
+        self.connection = connect(self.connstring)
+        AsyncProcess.main(self)
+
+    def tick(self):
+        self._execute()
 
     def quit(self):
         self.queue_command('quit')
 
     def _quit(self):
-        print "Stopping manager"
+        print "Stopping worker"
         self._running = False
+
 
     def execute(self):
         self.queue_command('execute')
 
     def _execute(self):
         print self.watch.script
+        status = STATUS_OK
+        old_status = self.watch.status
+
+        if old_status != status and old_status != STATUS_UNKNOWN:
+            print "STATUS CHANGED: %s -> %s" % (old_status, status)
+            
+
+class WorkerSet(object):
+    """ Object managing spawning of other workers."""
+    def __init__(self):
+        self._lock = Lock()
+        self.watches = {}
+        self.workers = {}
+        
+    def add(self, watch):
+        with self._lock:
+            name = watch.name
+            if name in self.watches:
+                raise KeyError("Watch `%s` already defined" % name)
+            print "Adding watch %s" % name
+            worker = WatchWorker(watch, self)
+            worker.spawn(True)
+            self.watches[name] = watch
+            self.workers[name] = worker
+
+    def remove(self, watch):
+        # support both strings and watch objects
+        with self._lock:
+            if hasattr(watch, 'name'):
+                name = watch.name
+            else:
+                name = watch
+
+            print "Removing worker %s" % name
+            if name in self.workers:
+                worker = self.workers[name]
+                worker.quit()
+            del self.workers[name]
+            del self.watches[name]
