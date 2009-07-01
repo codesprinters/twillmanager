3b28a2404edfb4105d52d4c2fec66603d301fcfb
pawel <pawel@charon.(none)>
2009-07-01 15:48:01 +0200

    Fix for bug #5155 in Python.

--- twillmanager/watcher.py
+++ twillmanager/watcher.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 import time
 from Queue import Empty
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, current_process
 
 # Consts for statuses
 STATUS_OK = 'OK'
@@ -17,28 +17,23 @@ class Watch(object):
         self.name = name
         self.interval = interval
         self.script = script
-
-class WatchStatus(object):
-    """ A simple data transfer object for describing watch statuses"""
-    def __init__(self, name, status = STATUS_UNKNOWN, time = None):
-        self.name = name
-        self.status = status # status
-        self.time = time # execution timestamp
+        self.status = STATUS_UNKNOWN # status
+        self.time = None# execution timestamp
 
 class AsyncProcess(object):
-    def __init__(self, daemon):
+    """ A simple abstract class for asynchronously responding server processes """
+    def __init__(self):
         self.queue = Queue(0)
-        self.process = Process(target=self.main)
-        self.process.daemon = daemon
+        self.process = current_process()
         self._running = False # this variable is not shared between processes
         
-    def start(self):
+    def spawn(self, daemon=False):
+        """ Start in another process """
+        self.process = Process(target=self.start)
+        self.process.daemon = daemon
         self.process.start()
 
-    def is_alive(self):
-        return self.process.is_alive()
-
-    def main(self):
+    def start(self):
         try:
             self._running = True
             while self._running:
@@ -52,7 +47,7 @@ class AsyncProcess(object):
         self.queue.put((name, arguments))
 
     def execute_command(self, cmd):
-        """ Execute a command queued by _queue_command """
+        """ Execute a command queued by `_queue_command` """
         name, arguments = cmd
         func = getattr(self, '_' + name)
         return func(*arguments)
@@ -60,30 +55,47 @@ class AsyncProcess(object):
 class WorkerManager(AsyncProcess):
     """ Object managing spawning of other workers."""
     def __init__(self):
-        AsyncProcess.__init__(self, False)
+        AsyncProcess.__init__(self)
+        self.watches = {}
         self.workers = {}
-        self.statuses = {}
 
     def add(self, watch):
+        """ Asynchronously add a watch.
+
+            :param watch: A `Watch` object
+        """
         self.queue_command('add', watch)
         
     def _add(self, watch):
         name = watch.name
-        if name in self.workers:
-            raise KeyError("Worker `%s` already defined" % name)
-        print "Adding worker %s" % name
+        if name in self.watches:
+            raise KeyError("Watch `%s` already defined" % name)
+        print "Adding watch %s" % name
+        worker = Worker(watch, self)
+        worker.spawn(True)
+        self.watches[name] = watch
+        self.workers[name] = worker
 
     def remove(self, watch):
+        """ Asynchronously remove a watch.
+
+            :param watch: A `Watch` object or watch name (string)
+        """
         self.queue_command('remove', watch)
 
     def _remove(self, watch):
-        name = watch.name
+        # support both strings and watch objects
+        if hasattr(watch, 'name'):
+            name = watch.name
+        else:
+            name = watch
+            
+        print "Removing worker %s" % name
         if name in self.workers:
-            print "Removing worker %s" % name
             worker = self.workers[name]
             worker.quit()
-            del self.workers[name]
-            del self.statuses[name]
+        del self.workers[name]
+        del self.watches[name]
 
     def quit(self):
         self.queue_command('quit')
@@ -91,30 +103,50 @@ class WorkerManager(AsyncProcess):
     def _quit(self):
         print "Stopping manager"
         self._running = False
+        for worker in self.workers.itervalues():
+            worker.quit()
+        self.workers = {}
+        self.watches = {}
 
     def update_status(self, name, status):
         self.queue_command('update_status', name, status)
 
     def _update_status(self, name, status):
-        if name not in self.statuses:
+        if name not in self.watches:
             return
-        assert self.statuses[name].name == name
-        self.statuses[name].status = status
-        self.statuses[name].time = time.time()
 
+        old_status = self.watches[name].status
+        self.watches[name].status = status
+        self.watches[name].time = time.time()
+        if old_status != STATUS_UNKNOWN and old_status != status:
+            print "STATUS CHANGED: %s -> %s" % (old_status, status)
+
+    def list(self):
+        back_queue = Queue()
+        self.queue_command('list', back_queue)
+
+        try:
+            return back_queue.get(True, 120)
+        except Empty:
+            return None
+    
+    def _list(self, back_queue):
+        back_queue.put(self.watches)
+            
 
 class Worker(AsyncProcess):
     """ Object managing spawning of other workers."""
     def __init__(self, watch, manager):
-        AsyncProcess.__init__(self, True)
+        AsyncProcess.__init__(self)
         self.watch = watch
         self.manager = manager
 
-    def main(self):
+    def start(self):
         try:
             self._running = True
             while self._running:
                 try:
+                    print self.queue
                     item = self.queue.get(True, self.watch.interval)
                     self.execute_command(item)
                 except Empty:
@@ -134,7 +166,3 @@ class Worker(AsyncProcess):
 
     def _execute(self):
         print self.watch.script
-
-if __name__ == '__main__':
-    manager = WorkerManager()
-    manager.start()
