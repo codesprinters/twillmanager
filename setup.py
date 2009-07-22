f04207b2c51003ae096fb473f5539556f7f587e8
pstradomski <pstradomski@codesprinters.com>
2009-07-22 18:49:09 +0200

    Adding twill to the repository.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from setuptools import setup, find_packages
 setup(
     name = "TwillManager",
-    version = "0.1-rc3",
+    version = "0.1-rc4",
     packages = find_packages(),
-    install_requires=["Cherrypy>=3.1.2", "Mako", "multiprocessing", "twill", "simplejson"],
+    install_requires=["Cherrypy>=3.1.2", "Mako", "multiprocessing", "simplejson"],
     tests_require=["nose", "mock"],
     zip_safe = False,
     include_package_data = True,
